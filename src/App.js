369e87b0d710ea8cf0738e90fd4485c638c02f71
kise <kaiazmi1412@gmail.com>
2021-09-11 02:47:32 +0530

    addingAboutViewReact#3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import AboutView from './components/About';
+
 
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
     <div>
       <Navbar />
       <Home />
+      <AboutView />
     </div>
   );
 }
