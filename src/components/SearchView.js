aee61a3cb4b434c9ae19b46fe3f11a41fb99db10
kise <kaiazmi1412@gmail.com>
2021-09-12 02:46:09 +0530

    madeTMDBacc

--- src/components/SearchView.js
+++ src/components/SearchView.js
@@ -1,5 +1,9 @@
 import Hero from "./Hero";
 
+// TMDB API KEY = b94fee07cfd9ff6aa88fffe47b128a64
+// Example link for movie searches = https://api.themoviedb.org/3/search/movie?api_key=b94fee07cfd9ff6aa88fffe47b128a64&language=en-US&query=arya&page=1&include_adult=false
+
+
 const SearchView = ({ keyword,searchReaults }) => {
   const title = `You are searching for ${keyword}`
   console.log(searchReaults, "are the search results")
